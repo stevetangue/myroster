83f5cec980f0d2b16b7f19b7defefaba048b7bb0
steveantangue <username@users.noreply.github.com>
2018-08-16 07:09:25 +1000

    cleane up moved to restructureData function

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Employees from './Employees';
 import EmployeesData from './json/employees';
 import Timezone from './json/config';
 import Roles from './json/roles';
-//import Shifts from '../json/shiftsSally'; for test
+//import SallyShifts from './json/shiftsSally';
 import Shifts from './json/shifts';
 import moment from 'moment-timezone';
 import { guid } from 'react-agenda';
@@ -27,9 +27,21 @@ let localOffset = now.getTimezoneOffset(); //-600
 let tzOffset = newdatetz.tz(Timezone.timezone)._offset; //480
 let timezoneOffset = localOffset + tzOffset;
 
-let itemsObj = [];
 
-Shifts.map((myshift, k) => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemsObjData: [],
+      isClicked: false,
+      classNameSelected: '',
+      employeeData: EmployeesData,
+    }
+  }
+
+  restructureData() {
+    let objData = [];
+    Shifts.map((myshift, k) => {
     // employee full name
     const employeeFullName = EmployeesData.map((emp) => {
         if(emp.id === myshift.employee_id) {
@@ -51,45 +63,23 @@ Shifts.map((myshift, k) => {
       return e !== undefined;
     });
 
-    // highlights sally in bright pink
-    //let roleID = myshift.employee_id === 2635 ? 4 : myshift.role_id;
-    //let roleID = myshift.role_id;
-
-    // Timezone config
-    //let startDateTz = moment(myshift.start_time);
-    //let endDateTz = moment(myshift.end_time);
-    //let newStartDate = startDateTz.tz(Timezone.timezone).format('YYYY-MM-DDTHH:mm:ss.SSSSZ');
-    //let newEndDate = endDateTz.tz(Timezone.timezone).format('YYYY-MM-DDTHH:mm:ss.SSSSZ');
-    // //new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0),
-    // console.log(newStartDate.toLocaleString('en-AU', { timeZone: 'Australia/Perth' }));
     let datenowstart = new Date(myshift.start_time);
     let datenowend = new Date(myshift.end_time);
-
-    itemsObj.push({
-      _id           : guid(),
-      name          : eFullName[0],
-      startDateTime : new Date(datenowstart.getFullYear(), datenowstart.getMonth(), datenowstart.getDate(), datenowstart.getHours() + timezoneOffset / 60, datenowstart.getMinutes()),
-      endDateTime   : new Date(datenowend.getFullYear(), datenowend.getMonth(), datenowend.getDate(), datenowend.getHours() + timezoneOffset / 60, datenowend.getMinutes()),
-      classes       : ' color-'+myshift.employee_id+'',
-      roleIs        : myshift.role_id,
-      roleName      : eRole[0],
-      employee_id   : myshift.employee_id,
-      shiftId       : myshift.id,
-      breakDuration : myshift.break_duration,
+      objData.push({
+        _id           : guid(),
+        name          : eFullName[0],
+        startDateTime : new Date(datenowstart.getFullYear(), datenowstart.getMonth(), datenowstart.getDate(), datenowstart.getHours() + timezoneOffset / 60, datenowstart.getMinutes()),
+        endDateTime   : new Date(datenowend.getFullYear(), datenowend.getMonth(), datenowend.getDate(), datenowend.getHours() + timezoneOffset / 60, datenowend.getMinutes()),
+        classes       : ' color-'+myshift.employee_id+'',
+        roleIs        : myshift.role_id,
+        roleName      : eRole[0],
+        employee_id   : myshift.employee_id,
+        shiftId       : myshift.id,
+        breakDuration : myshift.break_duration,
+      });
+      return <div></div>;
     });
-
-  return itemsObj;
-});
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      itemsObjData: itemsObj,
-      isClicked: false,
-      classNameSelected: '',
-      employeeData: EmployeesData,
-    }
+    return objData;
   }
 
   handleClick() {
@@ -97,13 +87,13 @@ class App extends Component {
       isClicked: true,
       classNameSelected: 'selected',
       itemsObjData: []
-    })
+    });
   }
 
   render() {
     const isClicked = this.state.isClicked;
     const employeeData = this.state.employeeData;
-    const itemsObjData = this.state.itemsObjData;
+    const itemsObjData = this.restructureData();
     const selected = this.state.classNameSelected;
     if(isClicked === true) {
       console.log(isClicked);
